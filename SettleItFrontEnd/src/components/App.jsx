099cd96f0bbcad7d3122821b806c7e43d50a5ce5
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-21 19:29:46 -0800

    Initial commit with all scaffolding set up

---
+++ SettleItFrontEnd/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="mainPage">
+      <style jsx>{`
+        .mainPage {
+          font-family: "Montserrat", sans-serif;
+          
+        }
+      @import url("https://fonts.googleapis.com/css?family=Montserrat");
+      `}
+      </style>
+      Home Page working!
+    </div>
+  );
+}
+
+
+export default App;
---
+++ SettleItFrontEnd/src/components/Seed.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Seed() {
+  return (
+    <div className="componentStyle">
+      <style jsx>{`
+        .componentStyle {
+            /* input CSS here*/
+                }
+            `}
+      </style>
+
+    </div>
+  );
+}
+
+export default Seed;
