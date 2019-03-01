be1ecb9c46169317402e1686e4bf8daab70072a1
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-01 13:35:13 -0800

    create button component for website

---
+++ SettleItFrontEnd/src/components/SettleItButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function SettleItButton({buttonText}) {
+  return (
+    <div className="componentStyle">
+      <style jsx>{`
+        .componentStyle {
+            /* input CSS here*/
+              }
+        button {
+        vertical-align: middle;
+        background-color: rgba(100,100,100,0.5);
+        border: 1px solid rgba(200,200,200,0.5);
+        padding: 1em;
+        border-radius: .5em;
+        color: white;
+        }
+
+        button:hover {
+        background-color: rgba(155,155,155,0.5);
+        }
+
+        button:active {
+          background-color: rgba(90,90,90,0.5);
+
+        }
+      `}
+      </style>
+        <button>{buttonText}</button>
+    </div>
+  );
+}
+
+SettleItButton.propTypes = {
+  buttonText: PropTypes.string
+}
+
+export default SettleItButton;
