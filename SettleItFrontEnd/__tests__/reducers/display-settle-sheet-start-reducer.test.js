1dd119dddabe2e232018634300e521d52b0bcd38
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-01 15:04:33 -0800

    add tests for redux

---
+++ SettleItFrontEnd/__tests__/reducers/display-settle-sheet-start-reducer.test.js
@@ -0,0 +1,36 @@
+import displaySettleSheetStartReducer from '../../src/reducers/display-settle-sheet-start-reducer';
+
+describe('displaySettleSheetReducer', () => {
+
+    let action;
+    const sampleSettleSheet = {
+        bandNames: ['Blue Ember', 'pseudoboss', 'The Heavy Hustle'],
+        dateSettleSheetCreated: '2018-02-26',
+        dateOfShow: '2019-02-26',
+        id: 0
+    };
+
+    test('Should return default state if no action type is recognized', () => {
+        expect(settleSheetReducer({}, { type: null })).toEqual({});
+    });
+
+    test('should successfully add new settle sheet data to masterSettleSheet list', () => {
+        const { bandNames, dateSettleSheetCreated, dateOfShow, id } = sampleSettleSheet;
+        action = {
+            type: 'ADD_NEW_SETTLE_SHEET',
+            bandNames: bandNames,
+            dateSettleSheetCreated: dateSettleSheetCreated,
+            dateOfShow: dateOfShow,
+            id: id
+        };
+
+        expect(settleSheetReducer({}, action)).toEqual({
+            [id]: {
+            bandNames: bandNames,
+            dateSettleSheetCreated: dateSettleSheetCreated,
+            dateOfShow: dateOfShow,
+            id: id
+            }
+        });
+    });
+});
