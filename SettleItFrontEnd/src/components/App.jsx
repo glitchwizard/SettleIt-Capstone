dc7031423109ecdc6a4b2096f3f04fa65944d8e4
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-01 15:07:30 -0800

    attempt to get modal toggle to work with redux

--- SettleItFrontEnd/src/components/App.jsx
+++ SettleItFrontEnd/src/components/App.jsx
@@ -2,15 +2,37 @@ import React from 'react';
 import { connect } from 'react-redux';
 import BackgroundImage from './BackgroundImage';
 import Header from './Header';
+import PropTypes from 'prop-types';
+import SettleItButton from './SettleItButton';
+import SettleSheetStartModal from './SettleSheetStartModal';
+import constants from './../constants';
 
 class App extends React.Component {
   constructor(props){
     super(props);
+    console.log('props');
+    console.log(props);
+    
+    this.handleShowSettleSheetModal = this.handleShowSettleSheetModal.bind(this);
   }
 
   componentDidMount() {
     
   }
+
+  handleShowSettleSheetModal() {
+    console.log('handleShowSettleSheetModal firing off');
+    event.preventDefault();
+
+    const action = {
+      type: constants.ADD_NEW_SETTLE_SHEET,
+      
+      showSettleSheetModal: true
+    };
+
+    dispatch(action);
+  } 
+
   render(){
     return (
       <div className="mainPage">
@@ -19,19 +41,37 @@ class App extends React.Component {
             font-family: "Montserrat", sans-serif;
           }
 
+          .hero {
+            display: relative;
+          } 
 
+          .SettleSheetButtonWrapper {
+            display: flex;
+            justify-content: center;
+            padding: 10px;
+          }
 
         @import url("https://fonts.googleapis.com/css?family=Montserrat");
         `}
         </style>
         <div>
-          <BackgroundImage />
           <Header />
+          <div className="hero">
+            <BackgroundImage />
+            {/* {settleSheetStartModal} */}
+            <div className="SettleSheetButtonWrapper" onClick={this.handleShowSettleSheetModal}>
+              <SettleItButton buttonText="Create Settle Sheet" />
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  showModal: PropTypes.string
+};
 
 
 export default connect()(App);
