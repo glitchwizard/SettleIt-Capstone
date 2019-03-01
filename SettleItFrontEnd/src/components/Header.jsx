479aa133382ed5ca5f6705cf8bb71ee080a2f179
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-01 11:34:27 -0800

    add login button

--- SettleItFrontEnd/src/components/Header.jsx
+++ SettleItFrontEnd/src/components/Header.jsx
@@ -6,18 +6,20 @@ function Header() {
     <div className="componentStyle">
       <style jsx>{`
         .componentStyle {
+          background-color: rgba(0, 0, 0, 0.4);
         }
 
         .header {
-          display: block;
-          background-color: rgba(0, 0, 0, 0.5);
+          display: flex;
           height: 4em;
+          border: 1px solid white;
+          justify-content: space-around;
+          align-items: center;
         }
 
         .bizIcon {
           height: 100%;
           display: inline-block;
-          margin-left: 10em;
         }
 
         img {
@@ -25,11 +27,37 @@ function Header() {
           display: inline-block;
           height: 3.5em;
         }
+
+        .buttonContainer {
+        }
+
+        button {
+          vertical-align: middle;
+          background-color: rgba(100,100,100,0.5);
+          border: 1px solid rgba(200,200,200,0.5);
+          padding: 1em;
+          border-radius: .5em;
+          color: white;
+        }
+
+        button:hover {
+          background-color: rgba(155,155,155,0.5);
+        }
+
+        button:active {
+          background-color: rgba(90,90,90,0.5);
+
+        }
+
+
       `}
       </style>
       <div className="header">
         <div className="bizIcon">
           <img src={settleItLogo} />
+        </div>
+        <div className="buttonContainer">
+          <button>Login</button>
         </div>
       </div>
     </div>
