b49467c4684a6bbe60938d4206d1cb6eab6442cf
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-01 09:25:01 -0800

    background image dialed in

--- SettleItFrontEnd/src/components/App.jsx
+++ SettleItFrontEnd/src/components/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import APIButton from './APIButton';
-
-
+import backgroundImage from '../assets/BO2A8527.jpg';
 
 class App extends React.Component {
   constructor(props){
@@ -12,7 +10,6 @@ class App extends React.Component {
   componentDidMount() {
     
   }
-
   render(){
     return (
       <div className="mainPage">
@@ -20,11 +17,42 @@ class App extends React.Component {
           .mainPage {
             font-family: "Montserrat", sans-serif;
           }
+
+          .bgImageContainerWrapper {
+              height: 100vh;
+              width: 100vw;
+              position: fixed;
+              overflow: hidden;
+          }
+          
+          .bgImageContainer {
+              height: 100%;
+              left: 50%;
+              position: absolute;
+              overflow: hidden;
+              transform: translate(-55%, -0%);
+            }
+          
+          img {
+            max-height: 100vw;
+            min-height: 800px;
+            min-width: 100vw;
+            transform: translate(-0%, -15%);
+            opacity: 0.9;
+          }
+
         @import url("https://fonts.googleapis.com/css?family=Montserrat");
         `}
         </style>
-        Home Page working!
-        <p />
+        <div>
+          <div className="bgImageContainerWrapper">
+            <div className="bgImageContainer">
+              <img src={backgroundImage} />
+            </div>
+          </div>
+
+
+        </div>
       </div>
     );
   }
