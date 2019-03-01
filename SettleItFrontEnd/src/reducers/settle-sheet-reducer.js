2927ef47905c99fe4a721a67c87a6f35590eecad
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-01 15:05:56 -0800

    refactoring for multiple cases

--- SettleItFrontEnd/src/reducers/settle-sheet-reducer.js
+++ SettleItFrontEnd/src/reducers/settle-sheet-reducer.js
@@ -1,10 +1,11 @@
 import constants from './../constants';
 
 export default (state = {}, action) => {
+  let newState;
+  const { bandNames, dateSettleSheetCreated, dateOfShow, id } = action;
   switch (action.type) {
   case constants.ADD_NEW_SETTLE_SHEET:
-    const { bandNames, dateSettleSheetCreated, dateOfShow, id } = action;
-    let newState = Object.assign({}, state, {
+    newState = Object.assign( {}, state, { 
       [id]: {
         bandNames: bandNames,
         dateSettleSheetCreated: dateSettleSheetCreated,
