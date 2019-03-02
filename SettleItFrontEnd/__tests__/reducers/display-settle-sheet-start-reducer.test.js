bb72a584dce1ab3e7e5d903464752d5315471509
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-01 16:01:57 -0800

    successfully toggling between display and hide

--- SettleItFrontEnd/__tests__/reducers/display-settle-sheet-start-reducer.test.js
+++ SettleItFrontEnd/__tests__/reducers/display-settle-sheet-start-reducer.test.js
@@ -1,36 +1,23 @@
 import displaySettleSheetStartReducer from '../../src/reducers/display-settle-sheet-start-reducer';
+import constants from './../../src/constants';
 
 describe('displaySettleSheetReducer', () => {
 
     let action;
-    const sampleSettleSheet = {
-        bandNames: ['Blue Ember', 'pseudoboss', 'The Heavy Hustle'],
-        dateSettleSheetCreated: '2018-02-26',
-        dateOfShow: '2019-02-26',
-        id: 0
-    };
 
     test('Should return default state if no action type is recognized', () => {
-        expect(settleSheetReducer({}, { type: null })).toEqual({});
+        expect(displaySettleSheetStartReducer({}, {
+            type: null
+        })).toEqual({});
     });
 
-    test('should successfully add new settle sheet data to masterSettleSheet list', () => {
-        const { bandNames, dateSettleSheetCreated, dateOfShow, id } = sampleSettleSheet;
+    test('Should return a positive toggle if DISPLAY_SETTLE_SHEET_START_TOGGLE is called', () =>{
         action = {
-            type: 'ADD_NEW_SETTLE_SHEET',
-            bandNames: bandNames,
-            dateSettleSheetCreated: dateSettleSheetCreated,
-            dateOfShow: dateOfShow,
-            id: id
-        };
+            type: constants.DISPLAY_SETTLE_SHEET_START_TOGGLE,
+        }
+        expect (displaySettleSheetStartReducer({}, action)).toEqual({
+            modalIsDisplayed: true
+        })
+    })
 
-        expect(settleSheetReducer({}, action)).toEqual({
-            [id]: {
-            bandNames: bandNames,
-            dateSettleSheetCreated: dateSettleSheetCreated,
-            dateOfShow: dateOfShow,
-            id: id
-            }
-        });
-    });
 });
--- SettleItFrontEnd/src/reducers/display-settle-sheet-start-reducer.js
+++ SettleItFrontEnd/src/reducers/display-settle-sheet-start-reducer.js
@@ -1,23 +1,22 @@
-import constants from '../constants'
+import constants from '../constants';
 
 export default (state = {}, action) => {
-    let newState;
-    const { modalIsDisplayed } = action;
+  let newState;
 
-    switch (action.type) {
-        case constants.DISPLAY_SETTLE_SHEET_START_TOGGLE:
-            newState =  Object.assign({}, state, {
-                modalIsDisplayed: modalIsDisplayed
-            });
-            return newState;
+  switch (action.type) {
+  case constants.DISPLAY_SETTLE_SHEET_START_TOGGLE:
+    newState =  Object.assign({}, state, {
+      modalIsDisplayed: true
+    });
+    return newState;
 
-        case constants.HIDE_SETTLE_SHEET_START_TOGGLE:
-            newState = Object.assign ({}, state, {
-                modalIsDisplayed: modalIsDisplayed
-            });
-            return newState;
+  case constants.HIDE_SETTLE_SHEET_START_TOGGLE:
+    newState = Object.assign ({}, state, {
+      modalIsDisplayed: null
+    });
+    return newState;
 
-        default: 
-            return state;
-    }   
-}
+  default: 
+    return state;
+  }   
+};
