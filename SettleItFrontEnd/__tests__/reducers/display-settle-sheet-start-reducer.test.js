29983787f7b0efd82293585ba7aa0840847ea720
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-07 13:58:20 -0800

    commit after running eslint --fix

--- SettleItFrontEnd/__tests__/reducers/display-settle-sheet-start-reducer.test.js
+++ SettleItFrontEnd/__tests__/reducers/display-settle-sheet-start-reducer.test.js
@@ -3,29 +3,29 @@ import constants from './../../src/constants';
 
 describe('displaySettleSheetReducer', () => {
 
-    let action;
+  let action;
 
-    test('Should return default state if no action type is recognized', () => {
-        expect(displaySettleSheetStartReducer({}, {
-            type: null
-        })).toEqual({});
+  test('Should return default state if no action type is recognized', () => {
+    expect(displaySettleSheetStartReducer({}, {
+      type: null
+    })).toEqual({});
+  });
+
+  test('Should return a positive toggle if DISPLAY_SETTLE_SHEET_START_TOGGLE is called', () =>{
+    action = {
+      type: constants.DISPLAY_SETTLE_SHEET_START_TOGGLE,
+    };
+    expect (displaySettleSheetStartReducer({}, action)).toEqual({
+      modalIsDisplayed: true
     });
+  });
 
-    test('Should return a positive toggle if DISPLAY_SETTLE_SHEET_START_TOGGLE is called', () =>{
-        action = {
-            type: constants.DISPLAY_SETTLE_SHEET_START_TOGGLE,
-        }
-        expect (displaySettleSheetStartReducer({}, action)).toEqual({
-            modalIsDisplayed: true
-        })
-    })
-
-        test('Should return a null value for toggle if HIDE_SETTLE_SHEET_START_TOGGLE is called', () => {
-            action = {
-                type: constants.HIDE_SETTLE_SHEET_START_TOGGLE,
-            }
-            expect(displaySettleSheetStartReducer({}, action)).toEqual({
-                modalIsDisplayed: null
-            })
-        })
+  test('Should return a null value for toggle if HIDE_SETTLE_SHEET_START_TOGGLE is called', () => {
+    action = {
+      type: constants.HIDE_SETTLE_SHEET_START_TOGGLE,
+    };
+    expect(displaySettleSheetStartReducer({}, action)).toEqual({
+      modalIsDisplayed: null
+    });
+  });
 });
