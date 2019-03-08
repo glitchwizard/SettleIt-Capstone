d5a08605cec0ee694148d24614469f117acce76c
Charley McGowan <glitchwizard@gmail.com>
2019-03-08 10:50:24 -0800

    successfully set up EFCore and commit data to the SQL server :-)

---
+++ SettleItAPI/Models/DataManager/SettleSheetManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SettleItAPI.Models.Repository;
+
+namespace SettleItAPI.Models.DataManager
+{
+  public class SettleSheetManager : IDataRepository<SettleSheet>
+  {
+    readonly SettleSheetContext _settleSheetContext;
+
+    public SettleSheetManager(SettleSheetContext context)
+    {
+      _settleSheetContext = context;
+    }
+
+    public IEnumerable<SettleSheet> GetAll()
+    {
+      return _settleSheetContext.SettleSheets.ToList();
+    }
+
+    public SettleSheet Get(long id)
+    {
+      return _settleSheetContext.SettleSheets.FirstOrDefault(e => e.SettleSheetId == id);
+    }
+
+    public void Add(SettleSheet entity)
+    {
+      _settleSheetContext.SettleSheets.Add(entity);
+      _settleSheetContext.SaveChanges();
+    }
+
+    public void Update(SettleSheet settleSheet, SettleSheet entity)
+    {
+      settleSheet.DateCreated = entity.DateCreated;
+      settleSheet.DateOfShow = entity.DateOfShow;
+      settleSheet.HeadlinerBand = entity.HeadlinerBand;
+      settleSheet.VenueName = entity.VenueName;
+
+      _settleSheetContext.SaveChanges();
+    }
+
+    public void Delete(SettleSheet settleSheet)
+    {
+      _settleSheetContext.SettleSheets.Remove(settleSheet);
+      _settleSheetContext.SaveChanges();
+    }
+  }
+}
---
+++ SettleItAPI/Models/SettleSheetContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SettleItAPI.Models
+{
+  public class SettleSheetContext : DbContext
+  {
+    public SettleSheetContext(DbContextOptions options)
+      : base(options)
+    { }
+
+    public DbSet<SettleSheet> SettleSheets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<SettleSheet>().HasData(
+        new SettleSheet
+        {
+          SettleSheetId = 1,
+          DateCreated = "2019-03-08",
+          DateOfShow = "2019-03-19",
+          HeadlinerBand = "Stargasm",
+          VenueName = "Doug Fir Lounge"
+        }, new SettleSheet
+        {
+          SettleSheetId = 2,
+          DateCreated = "2019-03-08",
+          DateOfShow = "2019-03-27",
+          HeadlinerBand = "Gabby Holt",
+          VenueName = "Dante's"
+        }      
+      );
+    }
+  }
+}
