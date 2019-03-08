5ced2aea626c1f58d8d078a88fe1f29a500495f2
Charley McGowan <glitchwizard@gmail.com>
2019-03-08 08:58:44 -0800

    upgrade jest to the latest version, also babel and everything else that needs to go with it for dependencies because it needs a lot of stuff apparently

--- SettleItFrontEnd/webpack.config.js
+++ SettleItFrontEnd/webpack.config.js
@@ -41,26 +41,22 @@ module.exports = {
                 emitWarning: true,
                 configFile: "./.eslintrc.json"
             }
-            },
-            {
-                test: /\.(png|gif|jp(e*)g|svg)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8000,
-                        name: 'images/[hash]-[name].[ext]'
-                    }
+        },
+        {
+            test: /\.(png|gif|jp(e*)g|svg)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 8000,
+                    name: 'images/[hash]-[name].[ext]'
                 }
-            },
-            {
+            }
+        },
+        {
             test: /\.jsx?$/,
             loader: "babel-loader",
             exclude: /node_modules/,
             options: {
-                presets: [
-                    ["es2015", {"modules": false}],
-                    "react",
-                ],
                 plugins: [
                     "react-hot-loader/babel",
                     "styled-jsx/babel"
