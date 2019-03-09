3a0b9d3b1c76236d254d6b0e89289ac4570406a0
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-08 17:11:22 -0800

    swap order for readability

--- SettleItFrontEnd/src/actions/index.js
+++ SettleItFrontEnd/src/actions/index.js
@@ -1,17 +1,17 @@
 import * as types from './../constants/ActionTypes';
 import v4 from 'uuid/v4';
 
-export const hideSettleSheetStart = () => ({
-  type: types.HIDE_SETTLE_SHEET_START_TOGGLE,
-  payload: {
-    isModalDisplayed: null     
-  }
-});
-
 export const displaySettleSheetStart = () => ({ 
   type: types.DISPLAY_SETTLE_SHEET_START_TOGGLE,
   payload: {
     isModalDisplayed: true
+  }
+});
+
+export const hideSettleSheetStart = () => ({
+  type: types.HIDE_SETTLE_SHEET_START_TOGGLE,
+  payload: {
+    isModalDisplayed: null     
   }
 });
 
