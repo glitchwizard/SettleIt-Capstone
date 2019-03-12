6987742f1c4733e25fdff038299c6d33ad44e666
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-11 19:57:31 -0700

    update Cors settings

--- SettleItAPI/Startup.cs
+++ SettleItAPI/Startup.cs
@@ -17,13 +17,24 @@ namespace SettleItAPI
       Configuration = configuration;
     }
 
+		readonly string SpecificOriginsAllowed = "_allowedSpecificOrigins";
+
     public IConfiguration Configuration { get; }
+
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-      services.AddCors();
+      services.AddCors(options =>
+			{
+				options.AddPolicy(SpecificOriginsAllowed, builder =>
+				{
+					builder.WithOrigins("http://localhost:8080")
+					.AllowAnyHeader()
+					.AllowAnyMethod();
+				});
+			});
 
       services.AddDbContext<SettleSheetContext>(options => options.UseSqlServer(Configuration["ConnectionString:SettleItDB"]));
       services.AddScoped<IDataRepository<SettleSheet>, SettleSheetManager>();
@@ -37,9 +48,7 @@ namespace SettleItAPI
         app.UseDeveloperExceptionPage();
       }
 
-      app.UseCors(builder => 
-      builder.WithOrigins("http://localhost:8080")
-      );
+      app.UseCors(SpecificOriginsAllowed);
 
       app.UseMvc();
     }
