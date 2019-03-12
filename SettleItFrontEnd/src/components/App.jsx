80a9355f9503fa495435f9f70cc08cf0bfec9d80
Charley McGowan <glitchwizard@gmail.com>
2019-03-12 10:06:07 -0700

    fix small format issues

--- SettleItFrontEnd/src/components/App.jsx
+++ SettleItFrontEnd/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import BackgroundImage from './BackgroundImage';
 import Header from './Header';
 import PropTypes from 'prop-types';
@@ -8,14 +8,14 @@ import * as actions from '../actions';
 import SettleSheetStartModal from './SettleSheetStartModal';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handleSettleSheetModalToggle = this.handleSettleSheetModalToggle.bind(this);
     this.handleGetSettleSheetsFromLocalAPI = this.handleGetSettleSheetsFromLocalAPI.bind(this);
   }
-  
+
   componentDidMount() {
-    
+
   }
 
   componentDidUpdate() {
@@ -23,22 +23,22 @@ class App extends React.Component {
   }
 
   handleSettleSheetModalToggle() {
-    const { dispatch } = this.props;
+    const {dispatch} = this.props;
     event.preventDefault();
     if (this.props.reduxState.settleSheetModalVisibility.isModalDisplayed) {
       dispatch(actions.hideSettleSheetStart());
     } else {
       dispatch(actions.displaySettleSheetStart());
     }
-  } 
+  }
 
   handleGetSettleSheetsFromLocalAPI() {
-    const { dispatch } = this.props;
+    const {dispatch} = this.props;
     event.preventDefault();
     dispatch(actions.getSettleSheetList());
   }
-  
-  render(){
+
+  render() {
     let divToDisplay;
     if (this.props.reduxState.settleSheetModalVisibility.isModalDisplayed) {
       divToDisplay = <div>
@@ -47,14 +47,14 @@ class App extends React.Component {
     } else {
       divToDisplay = null;
     }
-    
+
     let settleSheetsListRender;
 
     if (Object.keys(this.props.reduxState.settleSheets).length > 0) {
       settleSheetsListRender = Object.keys(this.props.reduxState.settleSheets).map(
         (settleSheetId) => {
           return <tr key={settleSheetId}>
-            <td className="tableRow">{ parseInt(settleSheetId) + 1 }</td>
+            <td className="tableRow">{this.props.reduxState.settleSheets[settleSheetId].settleSheetId}</td>
             <td className="tableRow">{this.props.reduxState.settleSheets[settleSheetId].dateCreated}</td>
             <td className="tableRow">{this.props.reduxState.settleSheets[settleSheetId].dateOfShow}</td>
             <td className="tableRow">{this.props.reduxState.settleSheets[settleSheetId].headlinerBand}</td>
@@ -65,7 +65,7 @@ class App extends React.Component {
     } else {
       settleSheetsListRender = null;
     }
-    
+
 
     return (
       <div className="mainPage">
@@ -92,12 +92,12 @@ class App extends React.Component {
             padding: 3px;
           }
 
-          th {
+          th, td {
             border: 1px solid black;
             border-radius: .25em;
             background-color: rgba(50,50,50,0.5);
             border: 1px solid rgba(200,200,200,0.5);
-            padding: 7px;
+            padding: 10px;
             text-align: center;
           }
 
@@ -131,12 +131,12 @@ class App extends React.Component {
             <BackgroundImage />
             {divToDisplay}
             <div className="centerThisItem" >
-              <SettleItButton buttonText="Create Settle Sheet" onClick={this.handleSettleSheetModalToggle}/>
+              <SettleItButton buttonText="Create Settle Sheet" onClick={this.handleSettleSheetModalToggle} />
               <br />
             </div>
             <div>
               <div className="centerThisItem">
-                <SettleItButton className="getBandsButton" onClick={this.handleGetSettleSheetsFromLocalAPI} buttonText="Get Settle Sheet List"/>
+                <SettleItButton className="getBandsButton" onClick={this.handleGetSettleSheetsFromLocalAPI} buttonText="Get Settle Sheet List" />
               </div>
               <div className="centerThisItem">
                 <table>
